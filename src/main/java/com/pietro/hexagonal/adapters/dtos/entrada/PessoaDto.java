86882864bd579306b913b98d8fdf031ae9656d26
Pietro <pietrobragaprofissional@gmail.com>
2025-09-24 21:25:31 -0300

    ajustes no pessoa dto e no application.yaml

--- src/main/java/com/pietro/hexagonal/adapters/dtos/entrada/PessoaDto.java
+++ src/main/java/com/pietro/hexagonal/adapters/dtos/entrada/PessoaDto.java
@@ -1,33 +1,36 @@
 package com.pietro.hexagonal.adapters.dtos.entrada;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class PessoaDto {
-
+    @NotBlank(message = "O Nome da Pessoa é obrigatório.")
     private String nome;
+    @NotBlank(message = "O Email da Pessoa é obrigatório.")
+    @Email(message = "Email inválido")
     private String email;
+    @NotBlank(message = "O Cpf da Pessoa é obrigatório.")
+    @Size(min = 11, max = 11, message = "O Cpf da Pessoa deve ter 11 dígitos.")
     private String cpf;
-
+    public PessoaDto() {
+    }
     public String getNome() {
         return nome;
     }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getCpf() {
         return cpf;
     }
-
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-
 }
